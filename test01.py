ba3d9ab7428d36581c3bd01368f77ab00e97937d
黄凯 <107037188+kai-huang1@users.noreply.github.com>
2022-06-08 09:13:00 +0800

    Update test01.py

--- test01.py
+++ test01.py
@@ -1,22 +1,12 @@
-from imp import reload
-
 import requests
-#
-# r = requests.get("http://www.baidu.com",timeout=1)
-# s = r.text
-# print(s)
-# st = "www."
-# en = ".png"
-# n = s.find(st)
-# f = s.find(en)
-# print(s[n:f + len(en)])
-#
-# print(r.elapsed.total_seconds())
-#
-# assert "//www.baidu.com/img/bd_logo1.png" in r.text
 
+r = requests.get("http://www.baidu.com",timeout=1)
+r.encoding = "utf-8"
+s = r.text
+st = "www."
+en = ".png"
+n = s.find(st)
+f = s.find(en)
+print(s[n:f + len(en)])
 
-r = requests.session().get("http://www.baidu.com")
-print(r.text)
-
-
+assert "//www.baidu.com/img/bd_logo1.png" in r.text
